5f3a5fe014f992961a13c051d659f4b958044e5b
Andreas Nägeli <andr.naegeli@gmail.com>
2020-03-11 20:14:46 +0100

    Remove multi-arch

--- Dockerfile.multi-arch.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker manifest create genhttp/gateway:latest genhttp/gateway:linux-x64 genhttp/gateway:linux-arm32 genhttp/gateway:linux-arm64 genhttp/gateway:windows-x64 genhttp/gateway:windows-arm32
-docker manifest push --purge genhttp/gateway:latest
