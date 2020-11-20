e191d4f5d74b7d6d7603ce1dcd7feed532075cbb
Andreas Nägeli <andr.naegeli@gmail.com>
2020-11-20 17:31:26 +0100

    Remove unecessary usings

--- GenHTTP.Gateway/Routing/FileOverlayBuilder.cs
+++ GenHTTP.Gateway/Routing/FileOverlayBuilder.cs
@@ -1,7 +1,6 @@
-﻿using GenHTTP.Api.Content;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System;
+
+using GenHTTP.Api.Content;
 
 namespace GenHTTP.Gateway.Routing
 {
